6d7113ab4103612323b12343e2e28a8b8f06a89b
Samir Marin <marin.samir@gmail.com>
2019-05-14 23:49:19 -0700

    Add insert queries from staging tables to analytic tables

--- sql_queries.py
+++ sql_queries.py
@@ -48,7 +48,7 @@ staging_songs_table_create= ("""CREATE TABLE IF NOT EXISTS staging_songs\
                                     artist_longitude numeric,\
                                     artist_name varchar,\
                                     duration numeric,\
-                                    num_songs varchar,\
+                                    num_songs int,\
                                     song_id varchar,\
                                     title varchar,\
                                     year int\
@@ -63,7 +63,7 @@ songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays\
                                 level varchar,\
                                 song_id varchar,\
                                 artist_id varchar,\
-                                sessions_id int,\
+                                session_id int,\
                                 location varchar,\
                                 user_agent varchar\
                             );\
@@ -127,20 +127,55 @@ staging_songs_copy = ("""COPY staging_songs \
 
 # FINAL TABLES
 
-songplay_table_insert = ("""
-""")
+songplay_table_insert = ("""INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) \
+                            SELECT DISTINCT (timestamp 'epoch' + (((e.ts)::bigint)/1000) * interval '1 second') AS start_time, \
+                                   e.userId AS user_id, \
+                                   e.level AS level, \
+                                   s.song_id AS song_id, \
+                                   s.artist_id AS artist_id, \
+                                   e.sessionId AS session_id, \
+                                   e.location AS location, \
+                                   e.userAgent AS user_agent \
+                            FROM staging_events e \
+                            JOIN staging_songs s ON (e.song=s.title AND e.artist=s.artist_name AND e.length=s.duration) \
+                            WHERE e.page='NextSong' \
+                        """)
 
-user_table_insert = ("""
-""")
+user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) \
+                        SELECT DISTINCT userId AS user_id, \
+                               firstName AS first_name, \
+                               lastName AS last_name, \
+                               gender, \
+                               level \
+                        FROM staging_events \
+                        WHERE page='NextSong' \
+                    """)
 
-song_table_insert = ("""
-""")
+song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) \
+                        SELECT DISTINCT song_id, title, artist_id, year, duration \
+                        FROM staging_songs \
+                     """)
 
-artist_table_insert = ("""
-""")
+artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude) \
+                          SELECT DISTINCT artist_id, \
+                                 artist_name AS name, \
+                                 artist_location AS location, \
+                                 artist_latitude AS latitude, \
+                                 artist_longitude AS longitude \
+                          FROM staging_songs \
+                       """)
 
-time_table_insert = ("""
-""")
+time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday) \
+                        SELECT DISTINCT (timestamp 'epoch' + (((ts)::bigint)/1000) * interval '1 second') AS start_time, \
+                               EXTRACT(hour from (timestamp 'epoch' + (((ts)::bigint)/1000) * interval '1 second')) AS hour, \
+                               EXTRACT(day from (timestamp 'epoch' + (((ts)::bigint)/1000) * interval '1 second')) AS day, \
+                               EXTRACT(week from (timestamp 'epoch' + (((ts)::bigint)/1000) * interval '1 second')) AS week, \
+                               EXTRACT(month from (timestamp 'epoch' + (((ts)::bigint)/1000) * interval '1 second')) AS month, \
+                               EXTRACT(year from (timestamp 'epoch' + (((ts)::bigint)/1000) * interval '1 second')) AS year, \
+                               EXTRACT(dayofweek from (timestamp 'epoch' + (((ts)::bigint)/1000) * interval '1 second')) AS weekday \
+                        FROM staging_events \
+                        WHERE page='NextSong' \
+                    """)
 
 # QUERY LISTS
 
@@ -148,3 +183,8 @@ create_table_queries = [staging_events_table_create, staging_songs_table_create,
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+
+#create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+#drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+#copy_table_queries = []
+#insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
